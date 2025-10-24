3a40b689959985939accff0a9c250e33807b55be
Kristopher Wilson <kristopherwilson@gmail.com>
2025-10-23 22:20:50 -0400

    fix: potential hack for GH pages + spa routing

---
+++ bin/ghPagesWorkaround.js
@@ -0,0 +1,30 @@
+// GitHUb pages, where I host this site, does not support single-page apps well, and will
+// throw a 404 if you refresh on a page or link directly to a page.
+// To workaround this, we're going to copy index.html to every single model page in dist
+// so that the app works as normal, even though its kind of gross.
+
+import fs from 'fs';
+import path from 'path';
+
+const distDir = path.resolve(import.meta.dirname, '..', 'dist');
+
+fs.mkdirSync(`${distDir}/model`, { recursive: true });
+
+const models = Object.values(
+  JSON.parse(
+    fs.readFileSync(
+      path.resolve(import.meta.dirname, '..', 'dist/models.json'),
+      'utf-8',
+    ),
+  ),
+).flat();
+
+for (const model of models) {
+  fs.mkdirSync(path.resolve(distDir, 'model', model.slug), { recursive: true });
+  fs.copyFileSync(
+    path.resolve(distDir, 'index.html'),
+    path.resolve(distDir, 'model', model.slug, 'index.html'),
+  );
+}
+
+// console.log(JSON.stringify(models, null, 2));
